551b6f420fe502f04245af9d653082045ede71eb
jeffchao98 <philadelphiafs@gmail.com>
2020-06-18 00:54:50 +0900

    MISC - Pre-created the empty kt files for the rest of the implementation

---
+++ WtrStkPractice/app/src/main/java/com/scchao/wtrstkpractice/ui/adapter/GridAdapter.kt
@@ -0,0 +1,2 @@
+package com.scchao.wtrstkpractice.ui.adapter
+
---
+++ WtrStkPractice/app/src/main/java/com/scchao/wtrstkpractice/ui/fragment/ListFragment.kt
@@ -0,0 +1,25 @@
+package com.scchao.wtrstkpractice.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ListFragment : Fragment() {
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
